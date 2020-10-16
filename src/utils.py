f977f67d5d917e4d77c84deabc4579233922db82
Yifan Wang <zbmsnj1@gmail.com>
2020-10-17 05:12:00 +1100

    delete Basedata class, combine to Planner class

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 
 #define the root path 
@@ -6,3 +7,8 @@ def get_project_root() -> Path:
     return Path(__file__).parent.parent
     
 
+ROOT_PATH = get_project_root() 
+TASK_PATH = os.path.join(ROOT_PATH, "src/task")
+DB_PATH_PATH = os.path.join(ROOT_PATH, "Database/path")
+BENCHMARK_PATH= os.path.join(ROOT_PATH, "benchmarks")
+DB_BENCHMARKS_PATH = os.path.join(ROOT_PATH, "Database/benchmarks.csv")
